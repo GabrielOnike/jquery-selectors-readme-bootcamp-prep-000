6157f94d5d0f4b18a410de9f83063401aaf079ff
Gabriel Onike <onikegabriel@gmail.com>
2019-10-28 14:55:22 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,6 +1,4 @@
 // declare your functions here...
-<script type = "text/javascript" language = "javascript">
-         $(document).ready(function paragraphSelector() {
-            $("p");
-         });
-      </script>
+$(document).ready(function() {
+    $("p");
+});
